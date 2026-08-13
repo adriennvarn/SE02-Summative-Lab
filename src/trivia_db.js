b2d5987b605f2f40c741631ccca0862272392695
Adrienn Varn <adriennvarn@gmail.com>
2026-08-13 15:40:04 -0400

    question database expanded

--- src/trivia_db.js
+++ src/trivia_db.js
@@ -18,5 +18,25 @@ export default [
         question: "In what country is the region of Catalonia?",
         correctAnswer: "Spain",
         wrongAnswers: ["France", "Italy", "Romania"]
+    },
+    {
+        question: "Which of these fast-food chains is NOT mainly known to sell pizza?",
+        correctAnswer: "Wendy's",
+        wrongAnswers: ["Domino's", "Little Caesar's", "Papa John's"]
+    },
+    {
+        question: "Which of the three astronauts of the Apollo 11 spaceflight did NOT walk on the moon?",
+        correctAnswer: "Michael Collins",
+        wrongAnswers: ["Buzz Aldrin", "Neil Armstrong", "None of them"]
+    },
+    {
+        question: "Who is the author of Jurassic Park?",
+        correctAnswer: "Michael Crichton",
+        wrongAnswers: ["Peter Benchley", "Chuck Paluhniuk", "Irvine Welsh"]
+    },
+    {
+        question: "What airline was the owner of the plane that crashed off the coast of Nova Scotia in 1998?",
+        correctAnswer: "Swiss Air",
+        wrongAnswers: ["Air France", "British Airways", "TWA"]
     }
 ]
